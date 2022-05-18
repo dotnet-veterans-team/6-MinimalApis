d9ed52e6de85f21459469ee58b37b06d7a52c6e2
Leonardo Silva <lsilva@alter-solutions.com>
2022-05-18 09:35:11 +0100

    initial commit

---
+++ Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalApiApp.Models
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; }
+
+        [Required, MinLength(3)]
+        public string Nome { get; set; }
+        public string Nif { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
